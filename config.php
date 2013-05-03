6872569d7a77e6c6f7f6f910269aa9d7c731edde
Jason Javier <jasonjavier06@gmail.com>
2013-05-03 12:48:17 +0800

    Yober Uploading File

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'uragonde_yoberDB');
+
+/** MySQL database username */
+define('DB_USER', 'uragonde_yoberDB');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'G@tY@ourNumber06');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc_');
+
+define('REL_WEB_URL', '/yober/');
+
+define('WEB_PATH', 'http://uragon-dev.com/yober/');
+
+?>
